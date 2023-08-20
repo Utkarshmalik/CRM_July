a168ebc678aaf5ad2b636636a2c315eefd62c099
utmalik <utmalik@amazon.com>
2023-08-20 20:14:00 +0530

    questions

---
+++ questions.js
@@ -0,0 +1,29 @@
+// Scopes
+// Execution Context 
+// TDZ
+// Shaddowing 
+// Let vs var vs const 
+
+// Protoypes 
+// Prototypal Inheritence 
+
+// Call Apply Bind 
+// Polfiils for call apply and bind 
+
+// Flatten an array
+// Flatten a object 
+
+// HOFs 
+// polyfills of map,filter,reduce, sort
+
+
+// Lexical Scoping
+// Closures
+// Currying 
+
+
+// Async programming 
+// event loop architecture 
+// Callbacks 
+// Promises 
+// Aync await 
